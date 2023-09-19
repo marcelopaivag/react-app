60f7a88852bc7c7022909159275782a9a1e80781
marcelopaivag <mff.paiva@gmail.com>
2023-09-19 20:16:54 -0300

    Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,27 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 export const Blog = () => {
   return (
-    <div>Blog</div>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Blog</h1>
+            <p>¡Bienvenido a nuestro blog! Aquí encontrarás artículos interesantes sobre diversos temas.</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h5>Últimas publicaciones</h5>
+            <ul className="list-unstyled">
+              <li><a href="#!">Título del artículo 1</a></li>
+              <li><a href="#!">Título del artículo 2</a></li>
+              <li><a href="#!">Título del artículo 3</a></li>
+              <li><a href="#!">Título del artículo 4</a></li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export const Contact = () => {
     return (
-        <h1>Contacto</h1>
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Contacto</h1>
+                        <p>¡Gracias por visitar nuestra página! Si tienes alguna pregunta o consulta, no dudes en contactarnos.</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h5>Información de contacto</h5>
+                        <ul className="list-unstyled">
+                            <li>Dirección: Av. Las Condes #12523. Local 1B. Las Condes, Santiago</li>
+                            <li>Teléfono: +56 9 1234 5678</li>
+                            <li>Correo electrónico: contacto@omtpaiva.cl</li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
