1506ab0dad46d801ba7efe079b305f4c19a1d794
marcelopaivag <mff.paiva@gmail.com>
2023-09-21 19:07:33 -0300

    asd

--- src/pages/ServiceDet.jsx
+++ src/pages/ServiceDet.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { ServiceDetComponent } from '../components/ServiceDetComponent'
+import { ContactComponent } from '../components/ContactComponent'
 
 export const ServiceDet = () => {
   return (
+    <>
     <ServiceDetComponent />
+    <ContactComponent />
+    </>
   )
 }
